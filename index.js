512903ea1cbb71fe6da7e24ed106b541b41ac437
adlifata <67993013+adlifata@users.noreply.github.com>
2021-10-04 19:05:36 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.BOT_TOKEN)
